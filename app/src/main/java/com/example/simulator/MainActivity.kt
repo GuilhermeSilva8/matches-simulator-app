2f3310b1d8baf4c1b747e84754e000e26cf06ce6
Guilherme <guilhermecsilva8@gmail.com>
2022-01-18 19:57:49 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas.

--- app/src/main/java/com/example/simulator/MainActivity.kt
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // adicionado pelo binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /* sem o binding seria assim
+        // setContentView(R.layout.activity_main)
+        // val tvHello = findViewById<TextView>(R.id.tvHello)
+         */
+
+        // com o binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // binding.tvHello.text = "Teste"
+
+
     }
 }
